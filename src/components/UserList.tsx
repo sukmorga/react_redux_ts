b1bc476a71a9110a0ae78f9c4bf0527dbdac5ec6
sukmorga <naginks@outlook.com>
2022-07-31 21:37:43 +0200

    todos logic

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,13 +1,12 @@
 import React, { FC } from 'react';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useActions } from '../hooks/useActions';
 import { useTypedSelector } from '../hooks/useTypedSelector';
-import { fetchUsers } from '../store/action-creators/user';
 
 const UserList: FC = () => {
     const { users, error, loading } = useTypedSelector(state => state.user);
     const { fetchUsers } = useActions()
+
     useEffect(() => {
         fetchUsers()
     }, [])
--- src/store/redusers/index.ts
+++ src/store/redusers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
+import { todoReduser } from "./todoReduser";
 import { userReduser } from "./userReduser";
 
 
 export const rootReduser = combineReducers({
     user: userReduser,
+    todo: todoReduser,
 })
 
 export type RootSate = ReturnType<typeof rootReduser>
